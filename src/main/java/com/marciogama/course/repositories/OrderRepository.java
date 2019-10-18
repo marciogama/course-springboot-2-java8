caf7dc3aa7e16203bc316ef713341ec558cc1d3c
marciogama <marciogama2000@gmail.com>
2019-10-18 08:50:44 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/marciogama/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.marciogama.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marciogama.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/marciogama/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.marciogama.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marciogama.course.entities.Order;
+import com.marciogama.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
